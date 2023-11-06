c8f4691022d88db66d4a26d781cd6d9eb7b54269
Mobb autofixer <git@mobb.ai>
2023-11-06 16:09:25 +0000

    mobb fix commit: 896bb636-8f79-4acf-bfc6-eec79a03d5a2

--- src/main/java/org/owasp/webgoat/lessons/pathtraversal/ProfileUploadRemoveUserInput.java
+++ src/main/java/org/owasp/webgoat/lessons/pathtraversal/ProfileUploadRemoveUserInput.java
@@ -33,6 +33,6 @@ public class ProfileUploadRemoveUserInput extends ProfileUploadBase {
   @ResponseBody
   public AttackResult uploadFileHandler(
       @RequestParam("uploadedFileRemoveUserInput") MultipartFile file) {
-    return super.execute(file, file.getOriginalFilename());
+    return super.execute(file, String.valueOf(file.getOriginalFilename()).replaceAll("([/\\\\:*?\"<>|])|(^\\s)|([.\\s]$)", "_").replaceAll("\0", ""));
   }
 }
